6073b94ada5bddf1afe6c344cd5a5b84048a7320
shreyanshanand <s.anand@queensu.ca>
2019-02-07 20:42:12 -0500

    created a noise maker

---
+++ NoiseMaker.py
@@ -0,0 +1,21 @@
+"""
+@author: Shreyansh Anand
+"""
+
+import random
+import numpy as np
+
+
+def add_noise(df):
+    # We add 0s randomly into the data in order to make
+    # noise which can then be used to train the model on
+    # NaN values.
+
+    matrix = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
+    # matrix = dataframe, used to randomly find rows and cols
+
+    for row, col in random.sample(matrix, int(round(.1 * len(matrix)))):
+        df.iat[row, col] = 0
+        # take the df and go into it at random rows and col and for 0.1% (10%)
+        # of them, make them into 0.
+    return df
