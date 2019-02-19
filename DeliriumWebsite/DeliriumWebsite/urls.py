08392e069195db339c497e1c9f16d6695637a815
shreyanshanand <s.anand@queensu.ca>
2019-02-18 23:56:54 -0500

    skeleton setup and tested

--- DeliriumWebsite/DeliriumWebsite/urls.py
+++ DeliriumWebsite/DeliriumWebsite/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+# Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+                  path('plot/', include('plot.urls')),
+                  path('', RedirectView.as_view(url='/plot/', permanent=True)),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# first path parameter is empty to imply '/'
--- DeliriumWebsite/Plotting/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlottingConfig(AppConfig):
-    name = 'Plotting'
